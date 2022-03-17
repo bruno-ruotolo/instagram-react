e6f24233a67c634cd0ca9ba70e58e301e80d46f1
Viviane Brenner <brenner.vivi@gmail.com>
2022-03-17 16:00:15 -0300

    Preparando revisao

--- src/FundoMobile.js
+++
@@ -1,9 +0,0 @@
-export default function FundoMobile() {
-  const ionIconInfomations = ["home", "search-outline", "add-circle-outline", "heart-outline", "person-outline"];
-
-  return (
-    <div class="fundo-mobile">
-      {ionIconInfomations.map(ionIconInfomation => <ion-icon name={ionIconInfomation}></ion-icon>)}
-    </div>
-  );
-};
